8f151d4c387ca12cb970d5e8d2b088ba25b1decb
trucocervejachurrasco <vomitevin018@gmail.com>
2025-08-24 13:18:44 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import ProductCard from '@/components/ProductCard';
 import { products } from '@/data/products';
-import heroBackground from '@/assets/hero-background.jpg';
+import heroBackground from '@/assets/header.png';
 import logo from '@/assets/logo2.png';
 
 const Index = () => {
